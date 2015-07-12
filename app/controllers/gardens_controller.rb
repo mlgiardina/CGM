ab7691c08b810f6dd1b4ccb2cb22d3bd32d937d3
Matthew Giardina <mlgiardina@me.com>
2015-07-12 17:54:14 -0500

    added basic html for garden overview

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,3 +1,9 @@
 class GardensController < ApplicationController
 
+  def index
+    authenticate_user!
+    @garden = current_user.garden if current_user
+    @plots = @garden.plots
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,26 +4,25 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-
-    if user && user.authenticate(params[:password])
-      if user.email_confirmed
+    if user.email_confirmed
+      if user && user.authenticate(params[:password])
         if params[:remember_me]
           cookies.permanent[:auth_token] = user.auth_token
         else
           cookies[:auth_token] = user.auth_token
         end
+        redirect_to root_url, notice: "Successfully logged in!"
       else
-        access_denied("Unconfirmed Email Address")
+        redirect_to login_path, alert: "Email or Password did not match."
       end
-      redirect_to root_url, notice: "Successfully logged in!"
     else
-      redirect_to login_path, alert: "Email or Password did not match."
+      redirect_to login_path, alert: "You must confirm your email address."
     end
   end
 
   def destroy
     cookies.delete(:auth_token)
-    redirect_to root_url, notice: "Successfully logged out. Goodbye!"
+    redirect_to login_path, notice: "Successfully logged out. Goodbye!"
   end
 
 end
