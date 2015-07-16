917ec5cd3ccdfbb7458528139267c734c41153c6
Matthew Giardina <mlgiardina@me.com>
2015-07-15 21:24:36 -0500

    configured production env to set the proper host as default url for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :mandrill
+  config.action_mailer.default_url_options = { host: "http://cgm2015.herokuapp.com/"}
 end
